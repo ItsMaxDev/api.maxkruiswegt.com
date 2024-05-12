75022b0b5c9fb1b4f701dda020f8a9c59b445c85
Max Kruiswegt <112982786+ItsMaxDev@users.noreply.github.com>
2024-05-12 19:44:06 +0200

    Fix the retrieval of the keystore

--- src/main/java/com/maxkruiswegt/api/security/JwtKeyProvider.java
+++ src/main/java/com/maxkruiswegt/api/security/JwtKeyProvider.java
@@ -6,9 +6,15 @@ import lombok.Getter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.File;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.Key;
 import java.security.KeyStore;
 import java.security.PublicKey;
+import java.util.Objects;
 
 @Configuration
 public class JwtKeyProvider {
@@ -34,11 +40,31 @@ public class JwtKeyProvider {
 
     @PostConstruct
     public void init() throws Exception {
-        ClassPathResource resource = new ClassPathResource(keystore);
-        KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-        keyStore.load(resource.getInputStream(), password.toCharArray());
+        InputStream keyStoreInputStream;
 
-        privateKey = keyStore.getKey(alias, password.toCharArray());
-        publicKey = keyStore.getCertificate(alias).getPublicKey();
+        // Check if the application is running from a JAR file
+        if (Objects.requireNonNull(JwtKeyProvider.class.getResource("")).toString().startsWith("jar:")) {
+            // Get the path of the JAR file
+            String jarPath = new File(JwtKeyProvider.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getParent();
+
+            // Build the path of the store.p12 file
+            Path keyStorePath = Paths.get(jarPath, keystore);
+
+            // Load the file
+            keyStoreInputStream = Files.newInputStream(keyStorePath);
+        } else {
+            // Load the file from the classpath
+            keyStoreInputStream = new ClassPathResource(keystore).getInputStream();
+        }
+
+        try {
+            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            keyStore.load(keyStoreInputStream, password.toCharArray());
+
+            privateKey = keyStore.getKey(alias, password.toCharArray());
+            publicKey = keyStore.getCertificate(alias).getPublicKey();
+        } finally {
+            keyStoreInputStream.close();
+        }
     }
 }
